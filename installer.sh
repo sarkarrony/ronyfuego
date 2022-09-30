d32f32b82515f468d63d26f50b7ced975716ea70
Sarkar Rony <111779556+sarkarrony@users.noreply.github.com>
2022-09-30 11:40:31 +0600

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install -y root-repo 
+pkg install -y git tsu python wpa-supplicant pixiewps iw
+
+git clone --depth 1 https://github.com/sarkarrony/ronyfuego ronyfuego
+
+chmod +x ronyfuego/ronyfuego.py
+
+printf "###############################################\n#  All done! Now you can run ronyfuego with\n#   sudo python ronyfuego.py -i wlan0 -K\n#\n#  To update, run\n#   (cd ShojibVaiSingle && git pull)\n###############################################\n"
