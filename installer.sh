57a46d76be5b8d149db2b144527513dfcd503bd6
Sarkar Rony <111779556+sarkarrony@users.noreply.github.com>
2022-09-30 15:34:32 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,4 +6,4 @@ git clone --depth 1 https://github.com/sarkarrony/ronyfuego ronyfuego
 
 chmod +x ronyfuego/ronyfuego.py
 
-printf "###############################################\n#  All done! Now you can run ronyfuego with\n#   sudo python ronyfuego.py -i wlan0 -K\n#\n#  To update, run\n#   (cd ShojibVaiSingle && git pull)\n###############################################\n"
+printf "###############################################\n#  All done! Now you can run ronyfuego with\n#   sudo python ronyfuego.py -i wlan0 -K\n#\n#  To update, run\n#   (cd ronyfuego)\n###############################################\n"
